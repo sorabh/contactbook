4b7a54eda69f8af93b6c5186ffc10aaf82d88fd7
sorabh <audace.sourabh@gmail.com>
2017-02-03 14:59:50 +0530

    update git repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.7.2'
 
 set :application, 'contactbook'
-set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:sorabh/contactbook.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
